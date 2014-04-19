e680162a44278a51a0f208ec4c381c158e20becb
wu06617106 <wu06617106@gmail.com>
2014-04-19 19:44:49 +0800

    classpath修改

--- HW1/src/LuceneIndex.java
+++ HW1/src/LuceneIndex.java
@@ -109,45 +109,5 @@ public class LuceneIndex{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		  /*int count = 0;
-		  FileInputStream fileInputStream = new FileInputStream("../../file/08.warc");
-		    // open our gzip input stream
-		    //GZIPInputStream gzInputStream=new GZIPInputStream(new FileInputStream(inputWarcFile));
-		    
-		    // cast to a data input stream
-		    DataInputStream inStream=new DataInputStream(fileInputStream);
-		    
-		    // iterate through our stream
-		    WarcRecord thisWarcRecord;
-		    while ((thisWarcRecord=WarcRecord.readNextWarcRecord(inStream))!=null) {
-		      // see if it's a response record
-		      if (thisWarcRecord.getHeaderRecordType().equals("response")) {
-		        // it is - create a WarcHTML record
-		        WarcHTMLResponseRecord htmlRecord=new WarcHTMLResponseRecord(thisWarcRecord);
-		        // get our TREC ID and target URI
-		        String thisTRECID=htmlRecord.getTargetTrecID();
-		        String thisTargetURI=htmlRecord.getTargetURI();
-		        String thisContentString = thisWarcRecord.getContentUTF8();
-		        org.jsoup.nodes.Document content = Jsoup.parse(thisContentString);
-
-		        String titleString = content.title();
-		        
-		        // print our data
-		        System.out.println(thisTRECID + " : " + thisTargetURI);
-		        System.out.println(titleString);
-		        
-			    org.jsoup.nodes.Document doc = Jsoup.parseBodyFragment(thisContentString);
-			    Element body = doc.body();
-			    String bodyTextString = body.text();
-			    String[] bodySplit = bodyTextString.split(" Content-Length: ");
-			    String bodyText = bodySplit[1];
-			    bodySplit = bodySplit[1].split(" ");
-			    bodyText = bodyText.substring(bodySplit[0].length() + 1, bodyText.length());
-			    System.out.println(bodyText);
-		      break;
-		      }
-		    }
-		    inStream.close();*/
-		    
 	} 
 }

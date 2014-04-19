d67cd810fff46e240e0171890ab6bcb939d9fbf7
Frank <tftf156@gmail.com>
2014-04-19 14:00:08 +0800

    整合完

--- HW1/src/LuceneIndex.java
+++ HW1/src/LuceneIndex.java
@@ -1,28 +1,115 @@
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Date;
 
 import javax.print.Doc;
 import javax.swing.text.Document;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Index;
+import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriter.MaxFieldLength;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class LuceneIndex{
 	
-	  private IndexWriter writer = null ; 
+	private IndexWriter writer = null ; 
+	static String filePath = "../../file/08.warc";
+	static String indexPath = "../../index";
+	static Analyzer analyzer = new StandardAnalyzer();
 	  
-	  public LuceneIndex() 
-	  { 
-	    
-	  } 
+	public LuceneIndex() 
+	{ 
+	} 
 	  
+	public static void createIndex() throws Exception {
+		FileInputStream fileInputStream = new FileInputStream(filePath);
+		// cast to a data input stream
+		DataInputStream inStream=new DataInputStream(fileInputStream);
+		Directory fsDir = FSDirectory.getDirectory(indexPath);
+	    //1、启动时读取
+	    Directory ramDir = new RAMDirectory(fsDir);
+	        
+	    // 运行程序时操作ramDir
+	    IndexWriter ramIndexWriter = new IndexWriter(ramDir, analyzer, MaxFieldLength.LIMITED);
+	        
+		// iterate through our stream
+		WarcRecord thisWarcRecord;
+		while ((thisWarcRecord=WarcRecord.readNextWarcRecord(inStream))!=null) {
+			// see if it's a response record
+			if (thisWarcRecord.getHeaderRecordType().equals("response")) {
+				// it is - create a WarcHTML record
+				WarcHTMLResponseRecord htmlRecord=new WarcHTMLResponseRecord(thisWarcRecord);
+				// get our TREC ID and target URI
+			 	String thisTRECID=htmlRecord.getTargetTrecID();
+				String thisTargetURI=htmlRecord.getTargetURI();
+				String thisContentString = thisWarcRecord.getContentUTF8();
+				org.jsoup.nodes.Document content = Jsoup.parse(thisContentString);
+	
+				String titleString = content.title();
+				        
+				// print our data
+				//System.out.println(thisTRECID + " : " + thisTargetURI);
+			    //System.out.println(titleString);
+				        
+			    org.jsoup.nodes.Document doc = Jsoup.parseBodyFragment(thisContentString);
+			    Element body = doc.body();
+			    String bodyTextString = body.text();
+			    String[] bodySplit = bodyTextString.split(" Content-Length: ");
+			    String bodyText = bodySplit[1];
+			    bodySplit = bodySplit[1].split(" ");
+			    bodyText = bodyText.substring(bodySplit[0].length() + 1 + titleString.length() + 1, bodyText.length());
+			    //System.out.println(bodyText);
+				    
+			    // 添加 Document
+			    org.apache.lucene.document.Document document = new org.apache.lucene.document.Document();
+			    //文件名称
+			    document.add(new Field("URL", thisTargetURI, Store.YES, Index.ANALYZED));
+			    //检索到的内容
+			    document.add(new Field("Title", titleString, Store.YES, Index.ANALYZED));
+			    //文件大小
+			    document.add(new Field("content", bodyText, Store.YES, Index.ANALYZED));
+			    ramIndexWriter.addDocument(document);
+			    //break;
+		    }
+		}
+		inStream.close();
+	    ramIndexWriter.close();
+	        
+	    //2、退出时保存
+	    IndexWriter fsIndexWriter = new IndexWriter(fsDir, analyzer, true, MaxFieldLength.LIMITED);
+	    fsIndexWriter.addIndexesNoOptimize(new Directory[]{ramDir});
+	        
+	    // 优化操作
+	    fsIndexWriter.commit();
+	    fsIndexWriter.optimize();
+	        
+	    fsIndexWriter.close();
+	}
 	  
-	  public static void main(String args[])throws IOException 
-	  { 
-		  int count = 0;
+	public static void main(String args[])throws IOException 
+	{ 
+		LuceneSearch luceneSearch = new LuceneSearch(indexPath, analyzer);
+		try {
+			Date start = new Date();
+			createIndex();
+			Date end = new Date(); 
+		    System.out.println("建立索引用時"+(end.getTime()-start.getTime())+"毫杪");
+			luceneSearch.search("Adult");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  /*int count = 0;
 		  FileInputStream fileInputStream = new FileInputStream("../../file/08.warc");
 		    // open our gzip input stream
 		    //GZIPInputStream gzInputStream=new GZIPInputStream(new FileInputStream(inputWarcFile));
@@ -60,7 +147,7 @@ public class LuceneIndex{
 		      break;
 		      }
 		    }
-		    inStream.close();
+		    inStream.close();*/
 		    
-	  } 
+	} 
 }
--- HW1/src/LuceneSearch.java
+++ HW1/src/LuceneSearch.java
@@ -3,20 +3,71 @@ import java.io.Console;
 import java.util.Date;
 import java.util.Scanner;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryParser.MultiFieldQueryParser;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.Filter;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+
 
 public class LuceneSearch {
 
 
+	String indexpath;
+    //分词器
+	Analyzer analyzer;
   
-  public LuceneSearch()
-  {
+	public LuceneSearch(String indexPath, Analyzer analyzer)
+	{
+		this.indexpath = indexPath;
+		this.analyzer = analyzer;
+	}
 
-  }
+	/**
+	* 搜索
+   	* 
+   	* IndexSearcher 用来在索引库中进行查询
+   	*/
+	public void search(String queryString) throws Exception {
+		//请求字段
+		//String queryString = "document";
 
+		// 1，把要搜索的文本解析为 Query
+		String[] fields = { "Title", "content" };
+		QueryParser queryParser = new MultiFieldQueryParser(fields, analyzer);
+		Query query = queryParser.parse(queryString);
+
+		// 2，进行查询，从索引库中查找
+		IndexSearcher indexSearcher = new IndexSearcher(indexpath);
+		Filter filter = null;
+		TopDocs topDocs = indexSearcher.search(query, filter, 10000);
+		System.out.println("总共有【" + topDocs.totalHits + "】条匹配结果");
+
+		int count = 1;
+		// 3，打印结果
+		for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+			// 文档内部编号
+			int index = scoreDoc.doc; 
+			// 根据编号取出相应的文档
+			Document doc = indexSearcher.doc(index);
+			System.out.println("------------------------------");
+			System.out.println("第" + count + "筆");
+			System.out.println("URL = " + doc.get("URL"));
+			System.out.println("Title = " + doc.get("Title"));
+			/*System.out.println("size = " + NumberTools.stringToLong(doc.get("size")));
+          	System.out.println("path = " + doc.get("path"));*/
+			count++;
+		}
+	}
   
-  public static void main(String[] args) throws Exception
-  {
+	public static void main(String[] args) throws Exception
+	{
 	
-  }
+	}
   
 }
